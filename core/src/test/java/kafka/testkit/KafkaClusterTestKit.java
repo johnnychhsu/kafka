eae7b4a83c74b0297ab771e7014b266662ea82c7
Johnny Hsu <johnnyhsu@fb.com>
2024-05-04 18:06:22 +0800

    remove check

--- core/src/test/java/kafka/testkit/KafkaClusterTestKit.java
+++ core/src/test/java/kafka/testkit/KafkaClusterTestKit.java
@@ -178,11 +178,8 @@ public class KafkaClusterTestKit implements AutoCloseable {
                         String.join(",", brokerNode.logDataDirectories()));
             } else {
                 // Set log.dirs equal to the metadata directory if there is just a controller.
-                if (controllerNode != null) {
-                    props.put(LOG_DIRS_CONFIG, controllerNode.metadataDirectory());
-                } else {
-                    props.put(LOG_DIRS_CONFIG, node.metadataDirectory());
-                }
+                props.put(LOG_DIRS_CONFIG,
+                    controllerNode.metadataDirectory());
             }
             props.put(SocketServerConfigs.LISTENER_SECURITY_PROTOCOL_MAP_CONFIG,
                     "EXTERNAL:PLAINTEXT,CONTROLLER:PLAINTEXT");
